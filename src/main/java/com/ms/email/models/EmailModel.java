c245a7fafd9fd8c2659ae77822483debfb17130f
mateusValentim6D76 <mateus.valentim19@hotmail.com>
2022-07-26 20:24:36 -0300

    Primeiro commit

---
+++ src/main/java/com/ms/email/models/EmailModel.java
@@ -0,0 +1,14 @@
+package com.ms.email.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "TB_EMAIL")
+public class EmailModel {
+
+	
+}
